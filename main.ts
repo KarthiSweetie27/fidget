d50316abc69f108ed8d45250e49fa5e969a3ce6d
KarthiSweetie27 <122539938+KarthiSweetie27@users.noreply.github.com>
2023-01-24 08:37:57 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,32 @@
-let Karthi = 0
+let sound_level = 0
+let temperature = 0
 input.onButtonPressed(Button.A, function () {
     basic.showString("Hello!")
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    sound_level = 255
+    music.playMelody("C5 E A D B C G F ", 120)
+})
+input.onSound(DetectedSound.Loud, function () {
+    basic.showLeds(`
+        # . . # .
+        # . # . .
+        # # . . .
+        # . # . .
+        # . . # .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("Karthi")
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Happy)
 })
 input.onGesture(Gesture.Shake, function () {
-    Karthi = 0
+    basic.showArrow(ArrowNames.NorthEast)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    temperature = input.temperature()
 })
 basic.forever(function () {
 	
